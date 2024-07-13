ad4a145d8d84edf95a087ce83c3b2fce5bbd7344
arpansaha1300 <Arpan.Saha3@amdocs.com>
2024-07-13 16:35:22 +0530

    sfsfs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,9 +10,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': ['warn'],
+
+    // TODO: temporarily disabled
+    'react/prop-types': 0,
+    'react/display-name': 0,
   },
 }
